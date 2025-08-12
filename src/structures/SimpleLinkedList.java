4b2cb842c16ee32eb518ea8e6d9a8bc50a7a6632
Moroz812 <antigenspb@gmail.com>
2025-08-12 18:21:20 +0300

    rename метода push в add в SimpleArrayList, добавлен static к вложенному класу Node, Node сделан самодостаточным метод toStrin g(), изменён мето toString() класса SimpleLinkedList, добавлен метод очистки clear(). удалён закомментированный бред и разсуждения при начальном подходе к реализации структуры

--- src/structures/SimpleLinkedList.java
+++ src/structures/SimpleLinkedList.java
@@ -9,7 +9,7 @@ public class SimpleLinkedList {
     private int size = 0;
 
     // класс Node (узел) - это "кирпичик", из которого состоит список.
-    private class Node { // это называется вложенный класс? нужно ли его делать static?
+    private static class Node { // это называется вложенный класс? нужно ли его делать static?
         private String data; // данные(значение узла)
         private Node next;   // ссылка на следующий узел (null если это последний узел)
 
@@ -17,6 +17,12 @@ public class SimpleLinkedList {
         public Node(String data) {
             this.data = data; // запись данных
             this.next = null; // не знаем, какой элемент будет следующим - null
+        }
+
+        // хорошая практика - сделать Node самодостаточным. Добавим ему toString():
+        @Override
+        public String toString() {
+            return data;  // возвращаем данные узла
         }
     }
 
@@ -65,7 +71,7 @@ public class SimpleLinkedList {
         Node current = head;
 
         while (current != null) {
-            sb.append(current.data);
+            sb.append(current); // Здесь автоматически вызовется current.toString() - но почему?
             if (current.next != null) {
                 sb.append(", ");
             }
@@ -76,6 +82,12 @@ public class SimpleLinkedList {
         return sb.toString();
     }
 
+    // метод очистки
+    public void clear() {
+        head = null; // обнуляем ссылки
+        size = 0;    // обнуляем размер списка
+    }
+
     // addFirst() ? метод который будет добавлять в первую ноду, а потом в начало списка
     // addLast() ? метод, который будет добавлять в конец списка
     // isEmpty() ? метод для проверки пустой ли список
@@ -84,103 +96,4 @@ public class SimpleLinkedList {
     // boolean contains() ? метод для проверки есть ли элемент в списке
     // remove() ? метод для удаления по значение
     // clear() ? метод очистки - head = null; size = 0; и если есть tail - tail = null;
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* всякий бред потом удалить
-
-    private static class Node<T> { //класс node
-        T data; // данные, нужна проверка на null?
-        Node<T> next; // ссылка на следующий объект?
-
-        // Конструктор
-        public Node (String data) {
-            this.data = data;
-        }
-        return data; //как возвращать String, если у нас класс типа объект?
-
-        //head Node; //первый узел в списке, если список пуст - null?
-        //tail Node; //последний узел (необязателен, но упрощает добавление в конец), если последний элемент пуст - null?
-    }
-
-    private Node<T> head; // первый узел связного списка
-    private Node<T> size; // длина списка
-
-    // проверка на пустоту
-    public boolean isEmpty() {
-        return head == null;  //
-    }
-
-    //
-    public void addFirst(String data) {
-        Node newNode = new Node(data);
-        newNode.next = head;  // Новый узел ссылается на старую голову
-        head = newNode;       // Обновляем голову
-    }
-
-
-    SimpleLinkedList {
-        head Node;
-        tail Node;
-
-        void push(String string) {
-            newNode = SimpleLinkedListNode(string, null);
-
-            tail.next = newNode;
-
-            tail = newNode;
-        }
-
-
-        void get (int index) {
-            Node temp;
-
-            for (i = 0; i < index; i++){
-                temp = temp.next;
-            }
-
-            return temp;
-        }
-
-    } */
-/*
-    //Добавление элемента
-    add(value) он же push
-
-    //Вставка элемента
-    add(index, value)
-
-    //Получение элемента
-    get(index)
-
-    //Изменение элемента
-    set(index, value)
-
-    //Удаление элемента
-    remove(index)
- */
-
